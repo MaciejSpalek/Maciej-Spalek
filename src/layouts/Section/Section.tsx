23372931ff752b269779e9607574aba6bb4047b4
Maciej Spałek <maciej.spalek@proexe.pl>
2024-06-02 20:30:40 +0200

    Removed gsap animations for mobile devices

--- src/layouts/Section/Section.tsx
+++ src/layouts/Section/Section.tsx
@@ -3,28 +3,32 @@ import { SectionProps } from "./Section.model";
 import { Container, HeadingWrapper, Heading, Content } from "./Section.styled";
 import { gsap } from "gsap";
 import { animationParams } from "helpers";
+import { useIsMobileView } from "hooks";
 
 export const Section = ({ title, children }: SectionProps) => {
+  const isMobileView = useIsMobileView();
   const headingRef = useRef(null);
 
   useLayoutEffect(() => {
-    const ctx = gsap.context(() => {
-      gsap.fromTo(
-        headingRef.current,
-        {
-          y: "+=100",
-          opacity: 0,
-        },
-        {
-          y: 0,
-          opacity: 1,
-          duration: animationParams.duration,
-        }
-      );
-    });
+    if (!isMobileView) {
+      const ctx = gsap.context(() => {
+        gsap.fromTo(
+          headingRef.current,
+          {
+            y: "+=100",
+            opacity: 0,
+          },
+          {
+            y: 0,
+            opacity: 1,
+            duration: animationParams.duration,
+          }
+        );
+      });
 
-    return () => ctx.revert();
-  }, []);
+      return () => ctx.revert();
+    }
+  }, [isMobileView]);
 
   return (
     <Container>
