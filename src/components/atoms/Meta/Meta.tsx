57a99c802c673d2f81822cd334794f306f36fe50
Maciej Spałek <maciej.spalek@proexe.pl>
2024-06-09 12:40:42 +0200

    Added favicon

--- src/components/atoms/Meta/Meta.tsx
+++ src/components/atoms/Meta/Meta.tsx
@@ -5,8 +5,33 @@ export const Meta = () => {
   return (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content="Discover my paintings, drawings, and photographs. Explore a gallery of unique art and stunning photography." />
+      <meta
+        name="description"
+        content="Discover my paintings, drawings, and photographs. Explore a gallery of unique art and stunning photography."
+      />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff"></meta>
+      <link rel="shortcut icon" href="../../../../public/favicon.ico" />
       <title>Maciej Spałek</title>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
     </Head>
   );
 };
