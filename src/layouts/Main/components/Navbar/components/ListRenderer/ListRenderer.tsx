db976909c42fd9a8555a11076583a64168f01e1c
Maciej Spałek <maciej.spalek@proexe.pl>
2024-06-10 20:40:22 +0200

    Added new photo, added accessability to MobileNavigation

--- src/layouts/Main/components/Navbar/components/ListRenderer/ListRenderer.tsx
+++ src/layouts/Main/components/Navbar/components/ListRenderer/ListRenderer.tsx
@@ -3,21 +3,34 @@ import { Anchor } from "./ListRenderer.styled";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
+interface INavigation {
+  href: string;
+  text: string;
+}
 interface IListRenderer {
-  navigation: any;
+  navigation: INavigation[];
   toggle: () => void;
+  isOpen?: boolean;
 }
 
-export const ListRenderer = ({ navigation, toggle }: IListRenderer) => {
+export const ListRenderer = ({
+  navigation,
+  toggle,
+  isOpen,
+}: IListRenderer): JSX.Element => {
   const { pathname } = useRouter();
 
-  return navigation.map(({ href, text }) => (
-    <li key={text}>
-      <Link href={href} passHref>
-        <Anchor onClick={toggle} isActive={pathname === href}>
-          {text.toUpperCase()}
-        </Anchor>
-      </Link>
-    </li>
-  ));
+  return (
+    <>
+      {navigation.map(({ href, text }) => (
+        <li key={text} aria-hidden={!isOpen}>
+          <Link href={href} passHref>
+            <Anchor onClick={toggle} isActive={pathname === href}>
+              {text.toUpperCase()}
+            </Anchor>
+          </Link>
+        </li>
+      ))}
+    </>
+  );
 };
